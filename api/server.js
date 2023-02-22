775212ad01c4d3c1aa24edd60ef3003ec955e105
murat-dede <murat1_dede@hotmail.com>
2023-02-22 13:53:16 +0300

    Server da hata aldım
    server.listen bir fonksiton değildir hatası alıyorum.

--- api/server.js
+++ api/server.js
@@ -1,3 +1,44 @@
 // SUNUCUYU BU DOSYAYA KURUN
+const express = require("express");
+const User = require("./users/model");
 
+const server = express();
+server.use(express.json());
+
+server.post("/api/users",(req,res)=>{
+    let user = req.body.user;
+    if (!user.name||!user.bio){
+        res
+        .status(400)
+        .json({message:"Lütfen kullanici icin bir name ve bio sağlayın"});
+    }else{
+   user
+   .insert(user)
+   .then((newUser)=>{
+        res
+        .status(201)
+        .json(newUser);
+       })
+       .catch((err)=>{
+        res
+        .status(500)
+        .json({message:"Veritabanına kaydedilirken bir hata oluştu"});
+       });
+    }
+
+});
+server.get("/api/users",(req,res)=>{
+    User
+    .find()
+    .then((users)=>{
+        res
+        .status(201)
+        .json({users:users})
+    })
+    .catch((err)=>{
+     res
+     .status(500)
+     .json({message:"Kullanıcı bilgileri alınamadı"})
+    });
+});
 module.exports = {}; // SERVERINIZI EXPORT EDİN {}
